8c5d8758378c3d8342a46ffb8d1299111b76bcde
Brenden Forward <bforward@umich.edu>
2023-05-16 01:39:41 +0000

    Add venv support for bash

--- bash/40-functions.sh
+++ bash/40-functions.sh
@@ -1,7 +1,10 @@
 # epic functions for bash
 
 # check if pandoc and xe-latex are installed
-if command -v pandoc >/dev/null 2>&1 && command -v xelatex >/dev/null 2>&1 && command -v cargo >/dev/null 2>&1 && cargo watch --help >/dev/null 2>&1; then
+if  command -v pandoc >/dev/null 2>&1 && \
+    command -v xelatex >/dev/null 2>&1 && \
+    command -v cargo >/dev/null 2>&1 && \
+    cargo watch --help >/dev/null 2>&1; then
   # convert markdown to pdf
   function md2pdf() {
     pandoc -f markdown+raw_tex --pdf-engine=xelatex -o $1.pdf $1.md
@@ -41,4 +44,44 @@ function cleartemps() {
     rm -rfv "$tempdir"
   done
   tempdirs=()
+}
+
+# Python venv management
+
+function mkvenv() {
+  local venvname=${1:-.venv}
+  if [ -d "$venvname" ]; then
+    echo "Venv $venvname already exists." >&2
+    return 1
+  fi
+  python3 -m venv "$venvname"
+  source $venvname/bin/activate
+  pip install --upgrade pip
+  if [ -f "requirements.txt" ]; then
+    pip install -r requirements.txt
+  else
+    touch requirements.txt
+  fi
+  if [ -d ".git" ]; then
+    echo "$venvname" >> .gitignore
+  fi
+}
+
+function venv() {
+  local venvname=${1:-.venv}
+
+  if [ -f "$venvname/bin/activate" ]; then
+    source $venvname/bin/activate
+  else
+    echo "No venv found." >&2
+  fi
+}
+
+function venv-save() {
+  local venvname=${1:-.venv}
+  if [ -f "$venvname/bin/activate" ]; then
+    pip freeze > requirements.txt
+  else
+    echo "No venv found." >&2
+  fi
 }
