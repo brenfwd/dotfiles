b3181675f43874bab9e7c89398d16d90fd3a46fc
Brenden Forward <bforward@umich.edu>
2024-11-13 00:01:52 -0500

    remove bun check for now

--- bash/load.sh
+++ bash/load.sh
@@ -10,17 +10,17 @@ esac
 script_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Ensure `bun` is available before continuing
-if ! command -v bun &> /dev/null; then
-  echo "Please install \`bun\` to use the dotfiles bash customization"
-  echo "https://bun.sh/"
-  alias ls='ls --color=auto'
-  alias la='ls -A'
-  alias ll='ls -alF'
-  alias l='ls -CF'
-  alias ..='cd ..'
-  alias ...='cd ../..'
-  return
-fi
+# if ! command -v bun &> /dev/null; then
+#   echo "Please install \`bun\` to use the dotfiles bash customization"
+#   echo "https://bun.sh/"
+#   alias ls='ls --color=auto'
+#   alias la='ls -A'
+#   alias ll='ls -alF'
+#   alias l='ls -CF'
+#   alias ..='cd ..'
+#   alias ...='cd ../..'
+#   return
+# fi
 
 # load all files in the bash directory starting with a number
 for file in "$script_path"/[0-9]*.sh; do
